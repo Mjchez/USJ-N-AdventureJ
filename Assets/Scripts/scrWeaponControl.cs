9841546e66c2df58deeb2ad3adac0b497245471d
Mjchez <51550417+Mjchez@users.noreply.github.com>
2020-10-19 16:54:30 -0300

    Aplicação de particula no disparo

---
+++ Assets/Scripts/scrWeaponControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrWeaponControl : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            anim.SetBool("Shoot",true);
+        }else{
+            anim.SetBool("Shoot",false);
+        }    
+    }
+}
