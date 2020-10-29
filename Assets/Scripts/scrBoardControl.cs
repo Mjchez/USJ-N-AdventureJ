9d71aa0bea3e924348e36d5bf537d9354a638fe8
Mjchez <51550417+Mjchez@users.noreply.github.com>
2020-10-29 16:45:06 -0300

    Sair do Carro Finalmente

--- Assets/Scripts/scrBoardControl.cs
+++ Assets/Scripts/scrBoardControl.cs
@@ -6,25 +6,30 @@ public class scrBoardControl : MonoBehaviour
 {
     public GameObject door;
     scrPlayerMove playerMove;
-
     public scrCarControl carControl;
+    public AudioSource sfxCarMotor;
+    public Transform carSeat;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-    public void TryingToBoard(){
-        playerMove.transform.parent = transform;
+    public bool TryingToBoard(){
+        playerMove.transform.parent = carSeat;
         playerMove.transform.localPosition = Vector3.zero;
-        playerMove.gameObject.SetActive(false);
+        playerMove.transform.localRotation=Quaternion.identity;
+        //playerMove.gameObject.SetActive(false);
         door.transform.rotation=Quaternion.Euler(0,0,0);
         carControl.onBoard=true;
+        playerMove.wantToBoard=null;
+        sfxCarMotor.Play();
+        return true;
 
     }
 
@@ -42,6 +47,7 @@ public class scrBoardControl : MonoBehaviour
 
             if(playerMove)
             {
+                sfxCarMotor.Pause();
                 door.transform.rotation=Quaternion.Euler(0,0,0);
                 playerMove.wantToBoard=null;
                 playerMove=null;
--- Assets/Scripts/scrPlayerMove.cs
+++ Assets/Scripts/scrPlayerMove.cs
@@ -10,12 +10,76 @@ public class scrPlayerMove : MonoBehaviour
     GameObject gamecamera;
     public GameObject cameraAim;
     public GameObject spine;
-
-    public delegate void WantToBoard();
+    public delegate bool WantToBoard();
     public WantToBoard wantToBoard;
+    public GameObject saiDoCarroObj;
+    public scrCarControl carControlScr;
+
+    public enum State{
+        Combat,
+        Board,
+        Died
+    }
+
+    public State state;
+
     void Start()
     {
         gamecamera = Camera.main.gameObject;
+        ChangeState();
+    }
+
+    IEnumerator Combat(){
+        while(state==State.Combat)
+        {
+            Vector3 cameraRelativeMov = gamecamera.transform.TransformDirection(mov);
+            rdb.velocity= new Vector3(cameraRelativeMov.x*4, rdb.velocity.y, cameraRelativeMov.z*4);
+            float radtogo = Vector3.Dot(transform.forward, -gamecamera.transform.right)*5;
+            
+            transform.Rotate(0,radtogo,0);
+            Vector3 locVel= transform.InverseTransformDirection(rdb.velocity).normalized;
+
+            anim.SetFloat("Walk", locVel.z);
+            anim.SetFloat("SideWalk", locVel.x + radtogo);
+            anim.SetFloat("Speed",rdb.velocity.magnitude + radtogo);
+
+            yield return new WaitForFixedUpdate();
+        }
+        ChangeState();
+    }
+
+    IEnumerator Board(){
+        rdb.isKinematic=true;
+        Collider[] cols = GetComponentsInChildren<Collider>();
+        foreach(Collider col in cols)
+        {
+            col.enabled =false;
+        }
+
+        while(state==State.Board)
+        {
+           yield return new WaitForFixedUpdate(); 
+        }
+        ChangeState();
+    }
+
+    IEnumerator Died(){
+        while(state==State.Died)
+        {
+           yield return new WaitForFixedUpdate(); 
+        }
+        ChangeState();
+    }
+
+    void ChangeState(){
+        StopAllCoroutines();
+        StartCoroutine(state.ToString());
+    }
+
+    void ChangeState(State mystate){
+        state = mystate;
+        StopAllCoroutines();
+        StartCoroutine(mystate.ToString());
     }
 
     void Update()
@@ -23,36 +87,36 @@ public class scrPlayerMove : MonoBehaviour
         mov = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
         cameraAim.transform.forward = gamecamera.transform.forward;
 
-        if(Input.GetButtonDown("Fire2")){
-            wantToBoard();
+        if(Input.GetKeyDown(KeyCode.E)){
+            if(wantToBoard()){
+                ChangeState(State.Board);
+            }
         }
 
+        if(Input.GetKeyDown(KeyCode.F)){
+            if(state == State.Board){
+                transform.localPosition = saiDoCarroObj.transform.localPosition;
+                carControlScr.onBoard = false;
+                rdb.isKinematic = false;
+                transform.localRotation = Quaternion.identity;
+                ChangeState(State.Combat);
+                
+                Collider[] cols = GetComponentsInChildren<Collider>();
+                foreach(Collider col in cols)
+                {
+                    col.enabled = true;
+                }
+            }
+        }
+
+        
+        
         if(Input.GetButtonDown("Fire1")){
             anim.SetBool("Shoot",true);
         }else{
             anim.SetBool("Shoot",false);
         }
     } 
-
-    private void FixedUpdate(){
-        
-        Vector3 cameraRelativeMov = gamecamera.transform.TransformDirection(mov);
-        rdb.velocity= new Vector3(cameraRelativeMov.x*4, rdb.velocity.y, cameraRelativeMov.z*4);
-        float radtogo = Vector3.Dot(transform.forward, -gamecamera.transform.right)*5;
-        
-        transform.Rotate(0,radtogo,0);
-        Vector3 locVel= transform.InverseTransformDirection(rdb.velocity).normalized;
-
-        anim.SetFloat("Walk", locVel.z);
-        anim.SetFloat("SideWalk", locVel.x + radtogo);
-        anim.SetFloat("Speed",rdb.velocity.magnitude + radtogo);
-    }
-
-
-    private void LateUpdate(){
-        
-    }   
-
 
     private void OnAnimatorIK(int layerIndex){
         anim.SetBoneLocalRotation(HumanBodyBones.Spine, cameraAim.transform.localRotation);
